5605334f3b7ae82d2400ed2ef40bf9b44cd80eeb
Sumedha Chowdhury <sumedhachowdhury76@gmail.com>
2026-01-16 21:02:29 +0530

    FakeVote.sol

--- FakeVote.sol
+++ FakeVote.sol
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
 /*
  * FakeVoteDetection - Demo smart contract for storing votes 
  * with simple on-chain duplicate/fake detection rules.
